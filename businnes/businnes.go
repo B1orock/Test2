3dbcd7d8aba98e160b800222d05d626e59899d8a
Konstantin <bonny1pop@gmail.com>
2024-04-23 12:11:22 +0300

    added businnes logick

---
+++ businnes/businnes.go
@@ -0,0 +1,11 @@
+package business
+
+import "Variant3/config"
+
+func Init(cfg *config.Config) {
+	// Инициализация бизнес-логики (например, подключение к базе данных)
+}
+
+func GetGreeting() string {
+	return "Hello web!"
+}
---
+++ pkg/go.mod
@@ -0,0 +1,3 @@
+module Varian_v2
+
+go 1.21
