eb5ed1bcd4373bb042b7a246c2c868303a96f431
Konstantin <bonny1pop@gmail.com>
2024-04-23 12:34:27 +0300

    full aplication

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"Varian_v2/api"
+	"Varian_v2/busines"
+	"Varian_v2/config"
+)
+
+func main() {
+	// Загрузить конфигурацию
+	cfg := config.LoadConfig()
+
+	// Инициализировать бизнес-логику
+	busines.Init(cfg)
+
+	// Создать API-роутер
+	router := api.CreateRouter(cfg)
+
+	// Запустить сервер
+	fmt.Println("Запуск сервера на порту", cfg.Port)
+	http.ListenAndServe(":"+cfg.Port, router)
+}
